0ad105efa8e12c0f44860d3a2c5cb1241f336576
gabdev95 <gabriele.mdemedeiros@gmail.com>
2022-06-03 12:23:34 -0300

    Adição de img para README

--- sintaxe_e_operadores/script.js
+++ sintaxe_e_operadores/script.js
@@ -34,4 +34,4 @@ function criaSegundaFrase(num1, num2) {
   return `Sua soma é ${soma}, que é ${resultado10} que 10 e ${resultado20} que 20.`;
 }
 
-console.log(comparaNumeros(1,2))
+console.log(comparaNumeros(20,10))
